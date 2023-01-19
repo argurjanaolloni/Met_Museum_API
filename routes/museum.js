e1a4d2a36f1d9a7b4aef79bac6c48090d3a267ac
argurjanaolloni <argurjanaolloni@gmail.com>
2023-01-19 12:15:42 -0500

    fixed museum routes

--- routes/museum.js
+++ routes/museum.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", controllers.getArtworks);
 router.get("/:id", controllers.getArtwork);
 router.post("/", controllers.createArtwork);
-router.put("/:id", controllers.updateArtwork);
+router.put("/:id/edit", controllers.updateArtwork);
 router.delete("/:id", controllers.deleteArtwork);
 
 export default router;
